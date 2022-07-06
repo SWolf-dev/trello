115ce57cdffd2c1bd49f3d61fd228e65d937330f
pham toi <toiphamasi79@gmail.com>
2022-07-06 14:09:27 +0700

    fist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="trello-master">
+     <nav className="navbar app">App bar</nav>
+     <nav className="navbar board">Board bar</nav>
+     <div className="board-columns">
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li className="item-image">
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+      <div className="column">
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img src="https://i.ytimg.com/vi/mJ-2Wp3cXQ8/maxresdefault.jpg"/>
+            Design and Research
+          </li>
+          <li>seconds</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
+     </div>
     </div>
   );
 }
